6079adc42088517b0e4674362d8a2ca023aa27f1
RAJAT <rjt9058@gmail.com>
2022-04-20 15:57:55 +0530

    connect mongodb and apolloServer

---
+++ quotesProject/schemGql.js
@@ -0,0 +1,11 @@
+import {gql} from 'apollo-server';
+
+
+const typeDefs = gql`
+type Query{
+    greet:String 
+}
+
+`
+
+export default typeDefs
--- quotesProject/server.js
+++ quotesProject/server.js
@@ -1,2 +1,33 @@
-import {ApolloServer, gql} from "apollo-server";
-import {ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core";
+import {ApolloServer} from 'apollo-server';
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
+import typeDefs from './schemGql.js';
+import resolvers from './resolvers.js';
+import { MONGO_URI } from './config.js';
+import mongoose from 'mongoose';
+
+
+
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log(`connected to mongodb`);
+})
+
+mongoose.connection.on("error",(err)=>{
+    console.log("error",err);
+})
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins:[
+        ApolloServerPluginLandingPageGraphQLPlayground
+    ]
+})
+
+server.listen().then(({url})=>{
+    console.log(`server is running on : ${url}`);
+})
